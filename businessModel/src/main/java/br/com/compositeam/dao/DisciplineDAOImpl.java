31c1cc9455911347b3afce6690f3326b7f8387e7
Gustavo Coelho <gutorc@hotmail.com>
2015-09-06 19:11:34 -0300

    Discipline working
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- businessModel/src/main/java/br/com/compositeam/dao/DisciplineDAOImpl.java
+++ businessModel/src/main/java/br/com/compositeam/dao/DisciplineDAOImpl.java
@@ -5,16 +5,25 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
 
 import br.com.compositeam.model.Discipline;
 
-public class DisciplineDAOImpl implements DisciplineDAO {
-
+@Service
+public class DisciplineDAOImpl implements DisciplineDAO{
 	private SessionFactory sessionFactory;
-	 
+	
+	
     public void setSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
+	
+	
     
 	public void save(Discipline p) {
 		// TODO Auto-generated method stub
@@ -28,6 +37,12 @@ public class DisciplineDAOImpl implements DisciplineDAO {
 	public List<Discipline> list() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public DisciplineDAO get(){
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		DisciplineDAO personDAO = context.getBean(DisciplineDAO.class);
+		return personDAO;
 	}
 
 }
