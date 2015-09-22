62acc8382a0a8d54a61aa4fabe56aad0c1fe6321
Gustavo Coelho <gutorc@hotmail.com>
2015-09-21 23:54:57 -0300

    Database
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ businessModel/resources/database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS gradPlanner; 
+
+CREATE TABLE `gradPlanner`.`Discipline` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cod` VARCHAR(45) NULL,
+  `name` VARCHAR(300) NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `gradPlanner`.`User` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(45) NULL,
+  `password` VARCHAR(300) NULL,
+  PRIMARY KEY (`id`));
