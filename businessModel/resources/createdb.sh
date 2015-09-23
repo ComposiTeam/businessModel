cc6c73e0f46e4563642f1394371927342d018aa9
Gustavo Coelho <gutorc@hotmail.com>
2015-09-23 00:52:31 -0300

    Refactoring
    It was added comments to the code
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- businessModel/resources/createdb.sh
+++ businessModel/resources/createdb.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
+#variables
+#set the container name
+containerName="mysqldb"
+rootPassword="root"
+containerTag="5.5"
+databaseSqlFile="database.sql"
 
-sudo docker stop mysqldb
-sudo docker rm mysqldb
-sudo docker run --name mysqldb -d -t -i -e MYSQL_ROOT_PASSWORD=root mysql:5.5
-ipAddress=$(sudo docker inspect mysqldb | grep "IPAddress" | cut -d"\"" -f4)
+#make sure that container will be a new container
+sudo docker stop "$containerName"
+sudo docker rm "$containerName"
+
+#run the container
+sudo docker run --name "$containerName" -d -t -i -e MYSQL_ROOT_PASSWORD="$rootPassword" mysql:"$containerTag"
+
+#Getting the ip address of the container
+ipAddress=$(sudo docker inspect "$containerName" | grep "IPAddress" | cut -d"\"" -f4)
 echo $ipAddress
+
+#Getting the old ip address in the hosts file (It wad done for ubuntu so It may need change for others distributions)
 oldIPAddress=$( grep "gradplannerBD" /etc/hosts | cut -d" " -f1)
 echo $oldIPAddress
+
+#replacing the old ip for the new one
+#sed does not allow to redirect the output for the same file
+#so It is needed to create a tmp file
 sed s/$oldIPAddress/$ipAddress/ < /etc/hosts > /etc/hosts.tmp
+#replacing the host file 
 mv /etc/hosts.tmp /etc/hosts
+
+#time needed for docker set up the container
 sleep 15
-mysql -h $ipAddress -P 3306 -u root -proot < "database.sql"
+#creating the database
+mysql -h $ipAddress -P 3306 -u root -proot < "$databaseSqlFile"
